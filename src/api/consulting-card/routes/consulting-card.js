457348a0942bb34646c1e2633eabfabd196dedcc
debbiechang18 <67890073+debbiechang18@users.noreply.github.com>
2023-07-31 17:09:52 -0400

    replace research-card with consulting-card

---
+++ src/api/consulting-card/routes/consulting-card.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * consulting-card router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::consulting-card.consulting-card');
---
+++ src/api/consulting-card/services/consulting-card.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * consulting-card service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::consulting-card.consulting-card');
